b366ee32f63b3a401d006484a6622dc448b3aa13
Harrison Sayre <hsayre42@gmail.com>
2024-12-09 20:22:17 -0500

    Started initial work on AbstractEnttiy class.

---
+++ Backend/src/main/java/com/example/Backend/models/AbstractEntity.java
@@ -0,0 +1,14 @@
+package com.example.Backend.models;
+
+public class AbstractEntity {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
