389eac581eb904554564ffce5d58c6b037afa22a
keithpohl82 <keith.pohl82@gmail.com>
2024-12-12 13:25:56 -0600

    Added friend and respond dto. updated user entity to name table users. removed generator from user as it is not needed

--- Backend/src/main/java/com/example/Backend/models/User.java
+++ Backend/src/main/java/com/example/Backend/models/User.java
@@ -2,8 +2,10 @@ package com.example.Backend.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User extends AbstractEntity {
     private String username;
     private String email;
