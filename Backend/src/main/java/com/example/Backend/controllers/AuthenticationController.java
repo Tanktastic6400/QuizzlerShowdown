9494c64c005236ddde782649e166b96fb7907520
Harrison Sayre <hsayre42@gmail.com>
2025-01-04 17:19:16 -0500

    Changed RequestMapping name for AuthenticationController and made it work with login and registration pages.

--- Backend/src/main/java/com/example/Backend/controllers/AuthenticationController.java
+++ Backend/src/main/java/com/example/Backend/controllers/AuthenticationController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@RequestMapping("/authenticationservice") //74?
+@RequestMapping("/authenticationservice") //74?
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class AuthenticationController {
 
