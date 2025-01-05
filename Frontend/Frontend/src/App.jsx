a32e0a2205c8a7c2029812f50952eecd7430a082
keithpohl82 <keith.pohl82@gmail.com>
2025-01-04 20:05:45 -0600

    Cleaned up home page for testing

--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -26,10 +26,10 @@ function App() {
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/quizdisplay" element={<QuizDisplay/>} />
+      <Route path="/quizselector" element={<QuizSelector/>} />
     </Routes>
     <Chatbox />
     <FriendList />
-    <QuizSelector/>
   </BrowserRouter>
   )
 }
