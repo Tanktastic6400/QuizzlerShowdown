d8d0a6a3c5d3e1c5c7e034337192d49486404e7d
Irina Sharapova <ir.ur.smirnova@gmail.com>
2025-01-15 23:02:33 -0500

    Add UserSearch form

--- Backend/src/main/java/com/example/Backend/controllers/UserController.java
+++ Backend/src/main/java/com/example/Backend/controllers/UserController.java
@@ -1,4 +1,21 @@
 package com.example.Backend.controllers;
 
+import com.example.Backend.models.User;
+import com.example.Backend.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/search/users")
+    public List<User> searchUsers(@RequestParam String username) {
+        return userRepository.findByUsernameContaining(username);
+    }
 }
