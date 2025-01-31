41360b7f981b421230c838d5c0a9d194c5ed8333
Irina Sharapova <ir.ur.smirnova@gmail.com>
2025-01-31 15:26:40 -0500

    add redirection to user profile

--- Frontend/Frontend/src/components/UserSearch.jsx
+++ Frontend/Frontend/src/components/UserSearch.jsx
@@ -1,23 +1,24 @@
-import React, { useState, useEffect} from "react";
-
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
-function UserSearch({loggedInUser}) {
+function UserSearch({ loggedInUser }) {
   const [username, setUsername] = useState("");
   const [users, setUsers] = useState([]);
-  
+  const navigate = useNavigate();
+
   function usersSearch() {
     fetch(`http://localhost:8080/userservice/search/users?username=${username}`)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            setUsers(data);
-        })
-        .catch(function(error) {
-            console.error('Error fetching data:', error);
-        });
-    };
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        setUsers(data);
+      })
+      .catch(function (error) {
+        console.error('Error fetching data:', error);
+      });
+  };
 
 
   const handleSendRequest = async (index) => {
@@ -25,26 +26,28 @@ function UserSearch({loggedInUser}) {
     const user2Id = user.id;
     const sender = loggedInUser;
     const user1Id = sender.id;
-    
-    
 
     console.log(`Friend ID: ${user2Id} Logged in user ID: ${user1Id}`);
 
-      const response = await fetch("http://localhost:8080/friendlist/send-request", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            user1Id,
-            user2Id
-          }), 
-      })
+    const response = await fetch("http://localhost:8080/friendlist/send-request", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user1Id,
+        user2Id
+      }),
+    })
   };
 
 
   function handleChange(event) {
     setUsername(event.target.value);
+  }
+
+  function handleNavigation(username) {
+    navigate(`/profile/${username}`);
   }
 
   return (
@@ -57,11 +60,15 @@ function UserSearch({loggedInUser}) {
       />
       <button className="search-button" onClick={usersSearch}>Search</button>
       <ul>
-      {users.map((foundUser, index) => (
+        {users.map((foundUser, index) => (
           <li key={foundUser.id}>
-            {foundUser.username}{" "}
+            <a href="#" onClick={() => handleNavigation(foundUser.username)}>
+              {foundUser.username}
+            </a>
+            {" "}
+            {/* {foundUser.username}{" "} */}
             <Button className="search-button" onClick={() => handleSendRequest(index)}>Send Request</Button>
-            
+
           </li>
         ))}
       </ul>
