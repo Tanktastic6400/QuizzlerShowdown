91f5bf66e9b77791718ebe4734c0b63550e98596
Irina Sharapova <ir.ur.smirnova@gmail.com>
2025-02-02 17:47:36 -0500

    review changed logined user

--- Backend/src/main/java/com/example/Backend/controllers/ReviewController.java
+++ Backend/src/main/java/com/example/Backend/controllers/ReviewController.java
@@ -3,6 +3,8 @@ package com.example.Backend.controllers;
 import com.example.Backend.models.Review;
 import com.example.Backend.models.User;
 import com.example.Backend.models.data.ReviewRepository;
+import com.example.Backend.services.AuthenticationService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,15 +15,27 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/reviews")
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true")
 public class ReviewController {
 
     @Autowired
     private ReviewRepository reviewRepository;
 
+    @Autowired
+    private AuthenticationService authenticationService;
+
     @PostMapping
-    public ResponseEntity<Review> createReview(@RequestBody Review review) {
+    public ResponseEntity<Review> createReview(@RequestBody Review review,  HttpSession session) {
         review.setCreatedAt(LocalDateTime.now());
+
+        User loggedInUser = authenticationService.getUserFromSession(session);
+
+        if (loggedInUser != null) {
+            review.setUsername(loggedInUser.getUsername());
+        } else if (review.getUsername() == null || review.getUsername().trim().isEmpty()) {
+            review.setUsername("Anonymous");
+        }
+
         Review savedReview = reviewRepository.save(review);
         return ResponseEntity.ok(savedReview);
     }
